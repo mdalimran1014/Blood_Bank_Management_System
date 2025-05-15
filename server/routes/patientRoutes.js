caee7044022e9632956c5b1abc34da216841ff50
Minhajul <minhajul5090@gmail.com>
2025-05-15 23:13:02 +0600

    added patientRoutes

---
+++ server/routes/patientRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const {
+  registerPatient,
+  getPatients,
+} = require('../controllers/patientController');
+
+router.post('/register', registerPatient);
+router.get('/', getPatients);
+
+module.exports = router;
