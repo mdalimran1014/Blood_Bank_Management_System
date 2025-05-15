1f9a8e71cb02f2996c1dea8843cb15cc3a75bc2a
Minhajul <minhajul5090@gmail.com>
2025-05-15 23:02:43 +0600

    addedloodrequestRoutes

---
+++ server/routes/bloodRequestRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const {
+  requestBlood,
+  getRequests,
+  updateRequestStatus,
+} = require('../controllers/bloodRequestController');
+
+router.post('/', requestBlood);
+router.get('/', getRequests);
+router.put('/:id', updateRequestStatus);
+
+module.exports = router;
