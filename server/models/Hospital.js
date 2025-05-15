cff06b56694ec4ec06e83aef225a42f7162cb076
Minhajul <minhajul5090@gmail.com>
2025-05-15 22:56:26 +0600

    added HospitalModel

---
+++ server/models/Hospital.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+const hospitalSchema = new mongoose.Schema({
+  name: String,
+  location: String,
+  phone: String,
+  email: { type: String, unique: true },
+}, { timestamps: true });
+
+export default mongoose.model('Hospital', hospitalSchema);
