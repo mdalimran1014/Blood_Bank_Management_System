0caf092a5fbfc904f30b082ba438073acc931119
Minhajul <minhajul5090@gmail.com>
2025-05-18 23:49:48 +0600

    added urlsdonor

---
+++ server/donor/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
+
+urlpatterns = [
+    path('donorlogin', LoginView.as_view(template_name='donor/donorlogin.html'), name='donorlogin'),
+    path('donorsignup', views.donor_signup_view, name='donorsignup'),
+    path('donor-dashboard', views.donor_dashboard_view, name='donor-dashboard'),
+    path('donate-blood', views.donate_blood_view, name='donate-blood'),
+    path('donation-history', views.donation_history_view, name='donation-history'),
+    path('make-request', views.make_request_view, name='make-request'),
+    path('request-history', views.request_history_view, name='request-history'),
+    
+    # ✅ Added logout path
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
+]
